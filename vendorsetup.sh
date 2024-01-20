a77b71935ee41758765537e5bca8d934b2a8059e
edward0181 <edwardroggeveen@gmail.com>
2024-01-20 13:31:47 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,86 +21,60 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
-	export ALLOW_MISSING_DEPENDENCIES=true
+    export TARGET_ARCH=arm64
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+    export OF_NO_RELOAD_AFTER_DECRYPTION=1
+    export FOX_DISABLE_APP_MANAGER=1
+    export OF_MAINTAINER=Edward0181
+    export FOX_VERSION=R11.1_2.3
+    export FOX_BUILD_TYPE="Test"
+    export FOX_VARIANT=A12
+    export OF_SCREEN_H=2400
+    export OF_STATUS_H=122
+    export OF_STATUS_INDENT_LEFT=80
+    export OF_STATUS_INDENT_RIGHT=80
+    export OF_HIDE_NOTCH=1
+    export OF_ALLOW_DISABLE_NAVBAR=0
+    export OF_PATCH_AVB20=1
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+    export FOX_REPLACE_BOOTIMAGE_DATE=1
+    export OF_CHECK_OVERWRITE_ATTEMPTS=1
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+    export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+    export OF_RUN_POST_FORMAT_PROCESS=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_ADVANCED_SECURITY=1
+    export FOX_NO_SAMSUNG_SPECIAL=1
+    export FOX_ENABLE_APP_MANAGER=1
+    export FOX_INSTALLER_DEBUG_MODE=1
+    export FOX_TARGET_DEVICES=a23xq
+    export OF_USE_GREEN_LED=0
+    export FOX_DELETE_AROMAFM=1
+    export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
+    export OF_FORCE_PREBUILT_KERNEL=1
+    export FOX_REPLACE_BUSYBOX_PS=0
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_USE_SED_BINARY=1
+    export OF_ENABLE_LPTOOLS=1
+    export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+    export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+    export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
+    export OF_UNBIND_SDCARD_F2FS=1
 
-	export OF_USE_GREEN_LED=0
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_ENABLE_APP_MANAGER=1
-	export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_ENABLE_LPTOOLS=1
-        export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
-        export FOX_DELETE_AROMAFM=1
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
-
-
-
-	# try to prevent potential data format errors
-	export OF_UNBIND_SDCARD_F2FS=1
-
-	# R12.1 Settings
-	export FOX_VERSION="R12.1"
-
-	# Screen Settings
-	export OF_SCREEN_H=2408
-	export OF_STATUS_H=100
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-	export OF_CLOCK_POS=1
-	export OF_ALLOW_DISABLE_NAVBAR=0
-
-	# MediaTek
-	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
-	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
-
-	# Magisk
-	function download_magisk(){
-		# Usage: download_magisk <destination_path>
-		local DEST=$1
-		if [ -n "${DEST}" ]; then
-			if [ ! -e ${DEST} ]; then
-				echo "Downloading the Latest Release of Magisk..."
-				local LATEST_MAGISK_URL=$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep browser_download_url | grep Magisk- | cut -d : -f 2,3 | tr -d '"')
-				mkdir -p $(dirname ${DEST})
-				wget -q ${LATEST_MAGISK_URL} -O ${DEST} || wget ${LATEST_MAGISK_URL} -O ${DEST}
-				local RCODE=$?
-				if [ "$RCODE" = "0" ]; then
-					echo "Successfully Downloaded Magisk to ${DEST}!"
-					echo "Done!"
-				else
-					echo "Failed to Download Magisk to ${DEST}!"
-				fi
-			fi
-		fi
-	}
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
-	download_magisk $FOX_USE_SPECIFIC_MAGISK_ZIP
-
-	# maximum permissible splash image size (in kilobytes); do *NOT* increase!
-	export OF_SPLASH_MAX_SIZE=130
-
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
-else
-	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
-		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
-	fi
+	# Let's see which are our build vars
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
 fi
