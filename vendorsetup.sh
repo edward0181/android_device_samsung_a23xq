dfc5898cb2f1e2710b133fd20a39fdb6912583bc
Edwin's <155349594+EdwinT2@users.noreply.github.com>
2024-01-20 20:33:07 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,5 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-git clone https://gitlab.com/HayateDevTH/avb_tool -b main out/host/linux-x86/bin
-sudo chmod +rwx out/host/linux-x86/bin/avbtool
 chmod a+x device/samsung/a23xq/mkbootimg
 add_lunch_combo twrp_a23xq-eng
