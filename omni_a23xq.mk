ff6e7bb17a23a5bb95de30a418f98258c440df44
edward0181 <edwardroggeveen@gmail.com>
2024-01-21 10:33:17 +0100

    Update and rename twrp_a23xq.mk to omni_a23xq.mk

--- omni_a23xq.mk
+++ omni_a23xq.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Inherit some common TWRP stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit device configuration
